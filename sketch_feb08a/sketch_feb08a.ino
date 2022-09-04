c14a7b2fbfaae3483bef762da861d8ebcc97c5e4
Romashka <rroma13579@gmail.com>
2022-09-03 21:54:54 -0300

    first commit

---
+++ sketch_feb08a/sketch_feb08a.ino
@@ -0,0 +1,30 @@
+
+
+
+void setup()
+
+{
+  pinMode (0,OUTPUT);
+ pinMode (1,OUTPUT);
+  pinMode (2,OUTPUT); 
+  pinMode (3,OUTPUT);
+   pinMode (4,OUTPUT);
+    pinMode (5,OUTPUT);
+     pinMode (6,OUTPUT);
+      pinMode (7,OUTPUT);
+  
+
+ 
+}
+void loop(){
+while(true){
+ digitalWrite(1,HIGH);
+  digitalWrite(4,HIGH);
+ delay(20000);
+  digitalWrite(1,LOW);
+   digitalWrite(4,LOW);}}
+ 
+
+
+
+ 
